e56703a66321b289532431fb5d25ee4857fb06a0
Baptiste Gerondeau <baptiste@gerondeau.net>
2016-11-27 16:13:37 +0100

    début du tryhard

--- Project_V1_SIAM_PASCAL_GERONDEAU/src/Game.cpp
+++ Project_V1_SIAM_PASCAL_GERONDEAU/src/Game.cpp
@@ -18,3 +18,25 @@ void Game::SetdisplayMod(unsigned int val)
 {
     m_displayMod = val;
 }
+
+
+void Game::Play()
+{
+    BITMAP* buffer=NULL;
+    Joueur rhino();
+    Joueur elephant();
+
+    BoardGame board();
+
+    int win=0;
+
+    while(!win)
+    {
+        if(rhino.play(GetdisplayMod() ,&board)) win=1;
+        else if(elephant.play(GetdisplayMod() ,&board)) win=2;
+        board.display(buffer, GetdisplayMod());
+        if(GetdisplayMod()) blit(buffer,source,0,0,0,0, SCREEN_W, SCREEN_H);
+    }
+
+
+}
